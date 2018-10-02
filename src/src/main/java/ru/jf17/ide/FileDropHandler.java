48c055fde45de71660f777b28f61ff6ad6be40a3
jf17 <javafirst17@gmail.com>
2018-10-02 14:06:31 +0300

    add *.hpp extention

--- src/src/main/java/ru/jf17/ide/FileDropHandler.java
+++ src/src/main/java/ru/jf17/ide/FileDropHandler.java
@@ -38,11 +38,12 @@ final class FileDropHandler extends TransferHandler {
         for (File file: files) {
             // do something...
 
-            String ext2 = FilenameUtils.getExtension(file.getAbsolutePath());
+            String ext2 = FilenameUtils.getExtension(file.getAbsolutePath()).toUpperCase();
 
-            if(ext2.equals("java")){ new JavaWindow(file);}
-            else if(ext2.equals("html")){new HTMLWindow(file);}
-            else if(ext2.equals("cpp")){new CppWindow(file);}
+            if(ext2.equals("JAVA")){ new JavaWindow(file);}
+            else if(ext2.equals("HTML")){new HTMLWindow(file);}
+            else if(ext2.equals("CPP")){new CppWindow(file);}
+            else if(ext2.equals("HPP")){new CppWindow(file);}
         }
         return true;
     }
