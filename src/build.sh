2d0c51d9e9756fe9c622683d737de25e5ee60eec
alex <javafirst17@gmail.com>
2022-01-12 20:35:00 +0300

    Create bash scripts.

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+javac -d ./classes -cp "target/dependency/rsyntaxtextarea-3.1.3.jar:target/dependency/commons-io-2.7.jar:target/dependency/autocomplete-3.1.2.jar" /home/alex/IdeaProjects/jide/src/src/main/java/ru/jf17/ide/*.java
---
+++ src/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -classpath ./classes:./target/dependency/rsyntaxtextarea-3.1.3.jar:./target/dependency/commons-io-2.7.jar:./target/dependency/autocomplete-3.1.2.jar ru.jf17.ide.Main
