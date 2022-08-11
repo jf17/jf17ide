7164e7ec63991d8d3dddc3fc4eec94cc79f858c7
Alex <javafirst17@gmail.com>
2022-08-12 01:08:41 +0300

    BugFix : Update file path in script

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-javac -d ./classes -cp "target/dependency/rsyntaxtextarea-3.1.3.jar:target/dependency/commons-io-2.7.jar:target/dependency/autocomplete-3.1.2.jar" ./src/main/java/ru/jf17/ide/*.java
+javac -d target/classes -cp "target/dependency/rsyntaxtextarea-3.1.3.jar:target/dependency/commons-io-2.7.jar:target/dependency/autocomplete-3.1.2.jar" ./src/main/java/ru/jf17/ide/*.java
