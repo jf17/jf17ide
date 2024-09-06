6a0b8449a3cbcf320e802100021884a8ea61bc2f
alex <javafirst17@gmail.com>
2024-09-06 18:18:14 +0300

    Update Find function

--- src/src/main/java/ru/jf17/ide/HTMLWindow.java
+++ src/src/main/java/ru/jf17/ide/HTMLWindow.java
@@ -158,6 +158,7 @@ public class HTMLWindow  extends JFrame {
                 if(e.getKeyCode() == KeyEvent.VK_F && e.isControlDown()){
                     String selectionStr = textArea.getSelectedText();
                     findField.setText(selectionStr);
+                    findField.requestFocusInWindow();
                 }else if(e.getKeyCode() == KeyEvent.VK_S && e.isControlDown()){
                     if(isOpen) {
                         try {
@@ -172,6 +173,62 @@ public class HTMLWindow  extends JFrame {
                     }else{
                         JOptionPane.showMessageDialog(null, "File is NOT open!");
                     }
+                }
+            }
+
+        });
+
+        findField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                // Get the text to find...convert it to lower case for eaiser comparision
+                String find = findField.getText().toLowerCase();
+                // Focus the text area, otherwise the highlighting won't show up
+                textArea.requestFocusInWindow();
+                // Make sure we have a valid search term
+                if (find != null && find.length() > 0) {
+                    Document document = textArea.getDocument();
+                    int findLength = find.length();
+                    try {
+                        boolean found = false;
+                        // Rest the search position if we're at the end of the document
+                        if (pos + findLength > document.getLength()) {
+                            pos = 0;
+                        }
+                        // While we haven't reached the end...
+                        // "<=" Correction
+                        while (pos + findLength <= document.getLength()) {
+                            // Extract the text from teh docuemnt
+                            String match = document.getText(pos, findLength).toLowerCase();
+                            // Check to see if it matches or request
+                            if (match.equals(find)) {
+                                found = true;
+                                break;
+                            }
+                            pos++;
+                        }
+
+                        // Did we find something...
+                        if (found) {
+                            // Get the rectangle of the where the text would be visible...
+                            Rectangle viewRect = textArea.modelToView(pos);
+                            // Scroll to make the rectangle visible
+                            textArea.scrollRectToVisible(viewRect);
+                            // Highlight the text
+                            textArea.setCaretPosition(pos + findLength);
+                            textArea.moveCaretPosition(pos);
+                            // Move the search position beyond the current match
+                            pos += findLength;
+                        }
+
+                    } catch (Exception exp) {
+                        exp.printStackTrace();
+                    }
+
+                    findField.requestFocusInWindow();
+
+                }
                 }
             }
 
